9f7849c41da26ea10b5005b9d75c538508d0ab10
Raphael Parodi <marseille.tutor-raphael@c1r19s1.marseille.42.school>
2025-08-04 14:47:38 +0200

    fix(plugins/clangd): adding the clangd configuration to work w/nvim 1.11.3

--- lua/plugins/clangdextension_config.lua
+++ lua/plugins/clangdextension_config.lua
@@ -45,8 +45,6 @@ function clangdextension_config_lua.setup()
 			border = "none",
 		},
 	})
-	require("clangd_extensions.inlay_hints").setup_autocmd()
-	require("clangd_extensions.inlay_hints").set_inlay_hints()
 end
 
 return clangdextension_config_lua
